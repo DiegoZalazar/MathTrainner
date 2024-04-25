cd64c4fca7ab0dc47b430e4a91c1ff27daeadab7
AdalDanielMP <halo.chift@gmail.com>
2024-04-24 21:47:08 -0600

    restructured project

--- app/src/main/java/mx/ipn/escom/TTA024/domain/model/Module.kt
+++
@@ -1,7 +0,0 @@
-package mx.ipn.escom.TTA024.domain.model
-
-class Module(
-    var id: Int = 0,
-    var enable: Boolean = false
-) {
-}
--- app/src/main/java/mx/ipn/escom/TTA024/ui/EstudianteUI/PathScreen.kt
+++ app/src/main/java/mx/ipn/escom/TTA024/ui/EstudianteUI/PathScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -26,7 +25,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
 import mx.ipn.escom.TTA024.R
-import mx.ipn.escom.TTA024.domain.model.Module
 import mx.ipn.escom.TTA024.domain.model.modules
 import mx.ipn.escom.TTA024.ui.theme.MathTrainerTheme
 
