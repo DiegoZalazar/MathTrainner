20001a574db0c12331a8b45644f14b5b920a3d69
AdalDanielMP <halo.chift@gmail.com>
2024-05-20 00:23:11 -0600

    Fixed Lecciones and Ejercicios Composable

--- app/src/main/java/mx/ipn/escom/tta047/ui/adminUI/AdminEjerciciosActivity.kt
+++ app/src/main/java/mx/ipn/escom/tta047/ui/adminUI/AdminEjerciciosActivity.kt
@@ -20,7 +20,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
@@ -223,6 +225,7 @@ fun DialogEliminarEjercicio(
                 modifier = Modifier
                     .fillMaxSize()
                     .background(Color.White)
+                    .verticalScroll(rememberScrollState())
             ) {
                 Spacer(modifier = Modifier.height(20.dp))
                 Column(
